1762c592967af99d000d49a7bfcdb0a7eb473177
blix111 <104617668+blix111@users.noreply.github.com>
2026-04-17 15:22:35 +0200

    Remove unnecessary comments and include page_common.h

--- src/ui/page_analog_scanner.h
+++ src/ui/page_analog_scanner.h
@@ -4,9 +4,7 @@
 extern "C" {
 #endif
 
-// On prévient le compilateur que la "boîte" page_pack_t existe quelque part, 
-// pas besoin de chercher sa taille tout de suite !
-typedef struct page_pack page_pack_t;
+#include "page_common.h"
 
 extern page_pack_t pp_analog_scanner;
 
