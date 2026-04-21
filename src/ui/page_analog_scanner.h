4352d4269c37c46fc7fdeca72bd6855cfa580a5b
blix111 <104617668+blix111@users.noreply.github.com>
2026-04-21 22:09:19 +0200

    Fix: move page_common.h include out of cplusplus block to fix build errors

--- src/ui/page_analog_scanner.h
+++ src/ui/page_analog_scanner.h
@@ -1,16 +1,15 @@
 #ifndef PAGE_ANALOG_SCANNER_H
 #define PAGE_ANALOG_SCANNER_H
 
+#include "page_common.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "page_common.h"
 
 extern page_pack_t pp_analog_scanner;
 
 #ifdef __cplusplus
 }
 #endif
-
 #endif
